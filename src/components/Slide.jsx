bbdd4a3be377e90f82c29c1209e3a3e8e7fca42e
methy <methymuy@gmail.com>
2025-08-24 13:22:13 +0700

    hhh

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -28,14 +28,12 @@ const slide = () => {
         }}
         navigation={true}
         modules={[Autoplay, Navigation]}
-        className=" overflow-y-auto  mySwipe h-64 mt-[20%] rounded-lg lg:w-2/4 lg:h-2/4 lg:mt-[10%]"
+        className=" overflow-y-auto  mySwipe h-64 mt-[35%] rounded-lg lg:w-2/4 lg:h-2/4 lg:mt-[10%]"
       >
-        <SwiperSlide><img  src={"https://systopup.com/admin-control/uploads/Top%20up%20banner-2.png"} /></SwiperSlide>
-        <SwiperSlide><img src={"https://systopup.com/admin-control/uploads/Top%20up%20banner-2.png"} /></SwiperSlide>
-        <SwiperSlide><img src={"https://systopup.com/admin-control/uploads/Top%20up%20banner-2.png"} /></SwiperSlide>
-        <SwiperSlide><img src={"https://systopup.com/admin-control/uploads/Top%20up%20banner-2.png"} /></SwiperSlide>
-        <SwiperSlide><img src={"https://systopup.com/admin-control/uploads/Top%20up%20banner-2.png"} /></SwiperSlide>
-        <SwiperSlide><img src={"https://systopup.com/admin-control/uploads/Top%20up%20banner-2.png"} /></SwiperSlide>
+        <SwiperSlide><img  src={"https://systopup.com/admin/uploads/689b3715d861c.jpeg"} /></SwiperSlide>
+        <SwiperSlide><img src={"https://systopup.com/admin/uploads/689b3715d861c.jpeg"} /></SwiperSlide>
+        <SwiperSlide><img src={"https://systopup.com/admin/uploads/689b3715d861c.jpeg"} /></SwiperSlide>
+      
         
       </Swiper>  
     </div>
